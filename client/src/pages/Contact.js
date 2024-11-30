e060590bef0da69eec773821976d46e25c439b2f
Nikhilkushwaa <nikhik15kumar@gmail.com>
2024-11-30 20:54:32 +0530

    adding client and server

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+const Contact = () => {
+  return (
+    <Layout>
+        
+        
+        
+        Contact
+        
+        
+        </Layout>
+  )
+}
+
+export default Contact
---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+const Dashboard = () => {
+  return (
+    <Layout title={"Dashboard - Ecommerce App"}>Dashboard</Layout>
+  )
+}
+
+export default Dashboard
